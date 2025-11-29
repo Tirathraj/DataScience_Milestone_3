a3f689baeb129fd53483c0b5c9508c494645981b
Simon Pino-Buisson <98675268+spynob@users.noreply.github.com>
2025-11-29 18:27:15 -0500

    page basic frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,44 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-"""
-General template for your streamlit app. 
-Feel free to experiment with layout and adding functionality!
-Just make sure that the required functionality is included as well
-"""
-
-st.title("INSERT APP TITLE HERE")
+st.title("Hockey Visualization App")
 
 with st.sidebar:
     # TODO: Add input for the sidebar
-    pass
+    workspace = st.selectbox(
+        "Workspace",
+        ("Home", "Work", "Train"),
+        )
+    model = st.selectbox(
+        "Model",
+        ("Distance", "Angle", "Distance + Angle"),
+    )
+    version = st.selectbox(
+        "Version",
+        ("1", "2"),
+    )
+    st.button("Get model", type="secondary")
 
 with st.container():
     # TODO: Add Game ID input
-    pass
+    game_id = st.selectbox(
+        "Game ID",
+        ("1", "2"),
+    )
+    st.button("Ping game", type="secondary")
 
+st.subheader("Game {ID}: {Team_1} vs {Team_2}")
 with st.container():
     # TODO: Add Game info and predictions
+    "Period {period_num} - {time} left"
+    left, right = st.columns(2)
+    with left.container():
+        "{team_1} xG (actual)"
+    with right.container():
+        "{team_2} xG (actual)"
     pass
 
 with st.container():
     # TODO: Add data used for predictions
+    st.subheader("Data used for predictions (and predictions)")
     pass
